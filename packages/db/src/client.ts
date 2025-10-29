683541c53dd1b719bab6dea92f77dc25984ad0b7
Dave Coen <david.a.coen@gmail.com>
2025-10-29 10:15:43 -0700

    More lint setup

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client'
 
 declare global {
   // allow global prisma during dev to avoid hot-reload issues
-  // eslint-disable-next-line no-var
+   
   var prisma: PrismaClient | undefined
 }
 
