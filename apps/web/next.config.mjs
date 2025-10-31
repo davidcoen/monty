1717bc33ac4f6f6edfbe73c4a2a864db5990a716
Dave Coen <david.a.coen@gmail.com>
2025-10-31 11:52:19 -0700

    Fix import errors

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,9 +1,21 @@
-// import { join } from 'path';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const config = {
+  experimental: { externalDir: true },
   typescript: { ignoreBuildErrors: false },
   eslint: { dirs: ['app', 'components', 'lib'] },
-  webpack: (cfg) => cfg
+  transpilePackages: ['@monty/lib'],
+  webpack: (cfg) => {
+    cfg.resolve = cfg.resolve || {};
+    cfg.resolve.alias = cfg.resolve.alias || {};
+    cfg.resolve.alias['@/packages/db'] = join(__dirname, '..', '..', 'packages/db/src');
+    cfg.resolve.alias['@monty/lib/plan'] = join(__dirname, '..', '..', 'packages/lib/plan/src');
+    return cfg;
+  },
 };
 
 export default config;
